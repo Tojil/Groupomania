7269c0850752fd0a2ef8a638f34534ad7cae0b1a
Sergio Canto Sabido <sergio@hanullimparis.com>
2021-11-07 17:52:41 +0100

    Démarrage

---
+++ reseau/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img id="logo" alt="Vue logo" src="./assets/icon-above-font.svg">
+  <HelloWorld msg="Bienvenue sur le Réseau !"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#logo {
+  width: 350px;
+}
+</style>
